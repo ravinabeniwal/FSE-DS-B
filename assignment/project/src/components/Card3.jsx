726c91d605681bfad364add07e5657dc4ccd1055
ravinabeniwal <ravinabeniwal390@gmail.com>
2026-07-31 13:23:26 +0530

    assignment2

---
+++ assignment/project/src/components/Card3.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card3 = () => {
+  return (
+    <div>
+      <div>
+        <h1>BURGER</h1>
+        <img src=" https://th.bing.com/th/id/OIP.S_fwoXNDM1aFS4K2fJfJ_gHaE8?w=248&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"alt="Burger" style={{borderRadius: '50%'}} />
+        <h3>Price :$100</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Card3
