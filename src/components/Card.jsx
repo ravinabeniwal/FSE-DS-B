126d325d243c0df892ba570657eb722ad7e05d97
unknown <ravinabeniwal390@gmail.com>
2026-09-24 09:59:16 +0530

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
-        <div style={{border: '2px solid white',height:'400px',width:'300px',backgroundColor:'DarkSlateGray', borderRadius: '10px'}}>
+        <div style={{border: '2px solid white',height:'450px',width:'300px',backgroundColor:'DarkSlateGray', borderRadius: '10px'}}>
             <h4 style={{ color: 'gold' }}>ABES STUDENT CARD</h4>
-            <img src="https://i.pinimg.com/originals/4f/ba/09/4fba09dde681e2db50ea2d2d57bbee90.jpg" alt="picture" style={{ width: '50%', height: 'auto',borderRadius: '5px' }} />
-            <h5 style={{ color: 'white' }}align="center" >Name: Ravina</h5>
-            <h5 style={{ color: 'white' }} align ="center">Class: Btech DS-B</h5>
-            <h5 style={{ color: 'white' }} align="center">Year: 3rd</h5>
+            <img src={props.imgSrc} alt="picture" style={{ width: '50%', height: 'auto',borderRadius: '7px' }} />
+            <h5 style={{ color: 'white' }}align="center" >{props.name}</h5>
+            <h5 style={{ color: 'white' }} align ="center">{props.class}</h5>
+            <h5 style={{ color: 'white' }} align="center">{props.rollNo}</h5>
         </div>        
     </div>
   )
